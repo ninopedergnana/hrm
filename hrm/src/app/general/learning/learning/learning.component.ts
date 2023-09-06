c68ec909a7ce3c9ebc8ccb82f9146d3ad0751775
ninopedergnana <pedergnana.nino@gmail.com>
2023-09-06 22:15:40 +0200

    finished tsting and learning tab with basic functionalities

--- hrm/src/app/general/learning/learning/learning.component.ts
+++ hrm/src/app/general/learning/learning/learning.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { DataHandlerService } from '../../services/data-handler.service';
+import { WordPair } from '../../models/WordPair';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { WordComparisonService } from '../../services/word-comparison.service';
 
 @Component({
   selector: 'app-learning',
@@ -8,9 +11,88 @@ import { DataHandlerService } from '../../services/data-handler.service';
 })
 export class LearningComponent implements OnInit {
 
-  constructor(private dataHandlerService: DataHandlerService) {}
+  questionWord: WordPair;
+  questionLanguages: { questionLanguage: string, answerLanguage: string }
+  questionLanguageWord = "";
+  answerLanguageWord = "";
+  questionForm: FormGroup;
+  trainingHasStarted: boolean;
+  answerIsFalse: boolean;
+  isNextButtonDisabled: boolean = true;
+  private completeInternalLearningData: WordPair[] = [];
+  private incorrectInternalLearningData: WordPair[] = [];
+  
+
+  constructor(
+    private dataHandlerService: DataHandlerService,
+    private fb: FormBuilder,
+    private wordComparisonService: WordComparisonService
+    ) {
+      this.questionForm = this.fb.group({
+        wordControl: new FormControl<string>('', Validators.required)
+      })
+    }
+
 
   ngOnInit(): void {
-    console.log(this.dataHandlerService.getGlobalTestWordPairList());
+    this.completeInternalLearningData = this.dataHandlerService.getGlobalTestWordPairList();
   }
+
+  startTraining(): void {
+    this.trainingHasStarted = true;
+    this.incorrectInternalLearningData = [...this.completeInternalLearningData];
+    this.setRandomQuestionWord();
+  }
+
+  checkAnswer(): void {
+    const writtenAnswer = this.questionForm.controls['wordControl'].value;
+     if (writtenAnswer === this.questionWord[this.questionLanguages.questionLanguage as keyof typeof this.questionWord]) {
+      this.handleCorrectfullAnswer();
+     } else {
+      this.handleWrongAnswer();
+     }
+  }
+
+  private handleWrongAnswer() {
+    console.log(this.incorrectInternalLearningData, 'before');
+
+    this.incorrectInternalLearningData = [...this.incorrectInternalLearningData, this.questionWord];
+    this.answerIsFalse = true;
+    this.isNextButtonDisabled = true;
+    console.log(this.incorrectInternalLearningData, 'after');
+    
+  }
+
+  private handleCorrectfullAnswer() {
+    console.log(this.incorrectInternalLearningData, 'before r');
+
+    const indexToRemove = this.incorrectInternalLearningData.findIndex(wp => wp.id === this.questionWord.id);
+    if (indexToRemove !== -1) {
+      this.incorrectInternalLearningData.splice(indexToRemove, 1);      
+    }
+    console.log(this.incorrectInternalLearningData, 'after r');
+
+    this.answerIsFalse = false;
+    this.isNextButtonDisabled = false;
+  }
+
+  setRandomQuestionWord(): void {
+    if (this.incorrectInternalLearningData?.length !== 0) {
+      this.isNextButtonDisabled = true;
+      this.questionForm.controls['wordControl'].patchValue('');
+      this.questionLanguages = this.wordComparisonService.getRandomTranslation();
+      const randomStartNumber = this.getRandomNumber(this.incorrectInternalLearningData.length);
+      this.questionWord = this.incorrectInternalLearningData[randomStartNumber];
+      this.questionLanguageWord = this.questionWord[this.questionLanguages.answerLanguage as keyof typeof this.questionWord] as string;
+      this.answerLanguageWord = this.questionWord[this.questionLanguages.questionLanguage as keyof typeof this.questionWord] as string;
+    } else {
+      this.trainingHasStarted = false;
+    }
+  }
+
+  getRandomNumber(maximum: number): number {
+    return Math.floor(Math.random() * maximum);
+  }
+
+
 }
--- hrm/src/app/general/testing/testing/testing.component.ts
+++ hrm/src/app/general/testing/testing/testing.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataHandlerService } from '../../services/data-handler.service';
+import { WordPair } from '../../models/WordPair';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { WordComparisonService } from '../../services/word-comparison.service';
 
 @Component({
   selector: 'app-testing',
@@ -6,5 +10,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./testing.component.css']
 })
 export class TestingComponent {
+
+  questionWord: WordPair;
+  questionLanguages: { questionLanguage: string, answerLanguage: string }
+  questionLanguageWord = "";
+  answerLanguageWord = "";
+  questionForm: FormGroup;
+  testingHasStarted: boolean;
+  showStats: boolean;
+  answeredCorrectly: number;
+  percentage: number;
+  answeredInCorrectly: number;
+  private completeInternalTestingData: WordPair[] = [];
+  private correctInternalLearningData: WordPair[] = [];
+  private incorrectInternalTestingData: WordPair[] = [];
+  
+
+  constructor(
+    private dataHandlerService: DataHandlerService,
+    private fb: FormBuilder,
+    private wordComparisonService: WordComparisonService,
+    ) {
+      this.questionForm = this.fb.group({
+        wordControl: new FormControl<string>('', Validators.required)
+      })
+    }
+
+
+  ngOnInit(): void {
+    this.completeInternalTestingData = this.dataHandlerService.getGlobalTestWordPairList();
+  }
+
+  startTest(): void {
+    this.wordComparisonService.setData(true);
+    this.completeInternalTestingData = this.dataHandlerService.getGlobalTestWordPairList();
+    this.showStats = false;
+    this.testingHasStarted = true;
+    this.incorrectInternalTestingData = [];
+    this.correctInternalLearningData = []
+    this.setRandomQuestionWord(true);
+  }
+
+  public showStatistics(): void {
+    this.showStats = true;
+    this.answeredCorrectly = this.correctInternalLearningData.length;
+    this.answeredInCorrectly = this.incorrectInternalTestingData.length;
+    const totalWords = this.answeredCorrectly + this.answeredInCorrectly;
+    this.percentage = (this.answeredCorrectly / totalWords) * 100;
+  }
+
+  checkAnswer(): void {
+    const writtenAnswer = this.questionForm.controls['wordControl'].value;
+     if (writtenAnswer === this.questionWord[this.questionLanguages.questionLanguage as keyof typeof this.questionWord]) {
+      this.handleCorrectfullAnswer();
+     } else {
+      this.handleWrongAnswer();
+     }
+     this.setRandomQuestionWord(false);
+  }
+
+  private handleWrongAnswer() {
+    //remove from question set
+    this.completeInternalTestingData = this.completeInternalTestingData.filter(wp => wp.id !== this.questionWord.id)
+    //add to wrong answer set
+    this.incorrectInternalTestingData = [...this.incorrectInternalTestingData, this.questionWord];
+  }
+
+  private handleCorrectfullAnswer() {
+    //remove from question set
+    this.completeInternalTestingData = this.completeInternalTestingData.filter(wp => wp.id !== this.questionWord.id)
+    //add to right answer set
+    this.correctInternalLearningData = [...this.correctInternalLearningData, this.questionWord];
+  }
+
+  setRandomQuestionWord(isInitialRandomWordCall: boolean): void {
+    if (this.completeInternalTestingData?.length !== 0) {
+      this.questionForm.controls['wordControl'].patchValue('');
+      this.questionLanguages = this.wordComparisonService.getRandomTranslation();
+      const randomStartNumber = this.getRandomNumber(this.completeInternalTestingData.length);
+      this.questionWord = this.completeInternalTestingData[randomStartNumber];
+      this.questionLanguageWord = this.questionWord[this.questionLanguages.answerLanguage as keyof typeof this.questionWord] as string;
+      this.answerLanguageWord = this.questionWord[this.questionLanguages.questionLanguage as keyof typeof this.questionWord] as string;
+    } else {
+      if(!isInitialRandomWordCall) {
+        this.showStatistics();
+      }
+      this.wordComparisonService.setData(false);
+      this.testingHasStarted = false;
+    }
+  }
+
+  getRandomNumber(maximum: number): number {
+    return Math.floor(Math.random() * maximum);
+  }
 
 }
