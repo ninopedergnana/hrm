ed55345edcd19de2006b779c9b92b0408fe373de
ninopedergnana <pedergnana.nino@gmail.com>
2023-09-06 22:14:29 +0200

    subscribe to testing boolean to disable buttons

--- hrm/src/app/app.component.ts
+++ hrm/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WordComparisonService } from './general/services/word-comparison.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hrm';
+  isTestingMode: boolean;
+
+  constructor(private wordComparisonService: WordComparisonService) {
+  }
+
+  ngOnInit(): void {
+    this.wordComparisonService.testingMode$
+    .subscribe((isTesting) => {
+      this.isTestingMode = isTesting;
+    });
+  }
+
+  
 }
